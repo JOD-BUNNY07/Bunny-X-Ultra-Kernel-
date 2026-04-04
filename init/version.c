a11f1180895d8a6a1101fd71f57f3ed5c93845ea
JOD-BUNNY07 <dassouradeep3310@gmail.com>
2026-04-04 17:25:29 +0530

    Update version.c

--- init/version.c
+++ init/version.c
@@ -42,9 +42,7 @@ EXPORT_SYMBOL_GPL(init_uts_ns);
 
 /* FIXED STRINGS! Don't touch! */
 const char linux_banner[] =
-	"Linux version " UTS_RELEASE " (" LINUX_COMPILE_BY "@"
-	LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
-
+    "Linux version NitroX-Zenith-KSUN (JOD_BUNNY@BUNNY-X) " UTS_VERSION "\n";
 const char linux_proc_banner[] =
 	"%s version %s"
 	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
